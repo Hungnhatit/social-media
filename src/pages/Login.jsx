9de020536491420a60fbb3447205a93108729ada
Hungnhatit <nhatpro204@gmail.com>
2024-08-17 00:25:22 +0700

    edit login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-export default function Login() {
+const Login = () => {
   return (
-    <>
+    <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center">
       <h1>This is Login page</h1>
-    </>
+    </div>
   )
-
 }
+
+export default Login;
