5af8c338971538777d6c165999c29e5cc876bb9e
Hungnhatit <nhatpro204@gmail.com>
2024-08-21 21:27:30 +0700

    update render components

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ import React from "react";
 import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
-export { TextInput, Loading, CustomButton }
+import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
+export { TextInput, Loading, CustomButton, TopBar, ProfileCard }
