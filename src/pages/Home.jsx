3ea280b04d7907efb41fcc70423a74335f6fae62
Hungnhatit <nhatpro204@gmail.com>
2024-08-21 21:28:29 +0700

    Update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { ProfileCard, TopBar } from "../components";
+
+
 export default function Home() {
   const { user } = useSelector((state) => state.user);
 
@@ -7,6 +10,26 @@ export default function Home() {
   return (
     <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
       <TopBar></TopBar>
+
+      <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
+        {/* Left */}
+        <div className="hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto">
+          <ProfileCard user={user}>
+            
+          </ProfileCard>
+        </div>
+
+
+
+
+        {/* Center */}
+        <div></div>
+
+
+
+        {/* Right */}
+        <div></div>
+      </div>
     </div>
   )
 }
