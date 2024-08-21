d201d52ee1c4454e37a26ff6efb7ca5176734b5f
Hungnhatit <nhatpro204@gmail.com>
2024-08-21 23:02:04 +0700

    Update profile card

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,8 +1,80 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
+import { NoProfile } from '../assets';
+import { LiaEditSolid } from 'react-icons/lia';
+import { UpdateProfile } from '../redux/userSlice';
+import { BsBriefcase, BsPersonFillAdd } from 'react-icons/bs';
+import { CiLocationOn } from 'react-icons/ci';
 
-const ProfileCard = () => {
+const ProfileCard = ({ user }) => {
+  const { user: data, edit } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   return (
-    <div>ProfileCard</div>
+    <div>
+      <div className='w-full bg-primary flex flex-col items-center shadow-sm rounded-xl px-6 py-4'>
+        <div className='w-full flex items-center justify-between border-b pb-5 border-[#66666645]'>
+          <Link to={'/profile/' + user?._id}
+            className='flex gap-2'>
+            <img
+              src={user?.profileUrl ?? NoProfile}
+              alt={user?.email}
+              className='w-14 h-14 object-cover rounded-full '
+            />
+            <div className='flex flex-col justify-center'>
+              <p className='text-lg font-medium text-ascent-2'>
+                {user?.firstName} {user?.lastName}
+              </p>
+              <span className='text-ascent-2'>
+                {user?.profession ?? "No Profession"}
+              </span>
+            </div>
+          </Link>
+
+          <div className=''>
+            {user?._id === data?._id ? (<LiaEditSolid
+              size={22}
+              className='text-blue cursor-pointer'
+              onClick={() => dispatch(UpdateProfile(true))}
+            ></LiaEditSolid>) : (
+              <button
+                className='bg-[#0444a430] text-sm text-white p-1 rounded'
+                onClick={() => { }}
+              >
+                <BsPersonFillAdd size={20} className='text-[#0f52b6]'></BsPersonFillAdd>
+              </button>
+            )}
+          </div>
+
+        </div>
+
+        <div className='w-full flex flex-col gap-2 py-4 border-b border-[#66666645]'>
+          <div className='flex gap-2 items-center text-ascent-1'>
+            <CiLocationOn className='text-xl text-ascent-1'></CiLocationOn>
+            <span>{user?.location ?? "Add a location"}</span>
+          </div>
+
+          <div className="flex gap-2 items-center text-ascent-1">
+            <BsBriefcase className="text-xl text-ascent-1"></BsBriefcase>
+            <span>{user?.profession ?? "Add a profession"}</span>
+          </div>
+        </div>
+
+        <div className='w-full flex flex-col gap-2 py-4 border-b border-[#66666645]'>
+          <p className='text-xl text-ascent-1 font-semibold'>
+            {user?.friends?.length} Friends
+          </p>
+
+          <div className='flex items-center justify-between'>
+            <span className='text-ascent-2'>Who viewed your profile?</span>
+            <span className='text-ascent-1 text-lg'>{user?.views?.length}</span>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
 
