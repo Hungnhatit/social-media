4b268c12cf4519c8152848557e534768ed5e11b0
Hungnhatit <nhatpro204@gmail.com>
2024-08-24 10:53:33 +0700

    Generate FriendCard

---
+++ src/components/FriendCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FriendCard = () => {
+  return (
+    <div>FriendCard</div>
+  )
+}
+
+export default FriendCard
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import TopBar from "./TopBar";
 import ProfileCard from "./ProfileCard";
-export { TextInput, Loading, CustomButton, TopBar, ProfileCard }
+import FriendCard from "./FriendCard";
+export { TextInput, Loading, CustomButton, TopBar, ProfileCard, FriendCard }
