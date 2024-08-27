5b4de735e23581582f5fcb413ce53ba4b7ae3b71
Hungnhatit <nhatpro204@gmail.com>
2024-08-27 23:03:04 +0700

    Update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,9 +29,9 @@ export default function Home() {
             <div className="w-full flex items-center gap-4 py-4 border-b border-[#66666645]">
               <img
                 src={user?.profileUrl ?? NoProfile}
-                alt="User Image" 
-                  className="w-14 h-14 rounded-full object-cover"
-                />
+                alt="User Image"
+                className="w-14 h-14 rounded-full object-cover"
+              />
             </div>
           </form>
 
