f8b44b57263323b9e4b2fad267b73676da9a6d4f
Hungnhatit <nhatpro204@gmail.com>
2024-09-01 17:17:39 +0700

    Update comment ui

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,19 +5,87 @@ import moment from 'moment';
 import { BiLike, BiSolidLike } from 'react-icons/bi';
 import { GoCommentDiscussion, GoShareAndroid } from 'react-icons/go';
 import { MdOutlineDeleteOutline } from 'react-icons/md';
+import { FcLike } from 'react-icons/fc';
+import { useForm } from 'react-hook-form';
+import { Loading, TextInput } from '../components';
+import { FaCircleArrowUp } from 'react-icons/fa6';
+import { postComments } from '../assets/data';
+
+// Comment form
+const CommentForm = ({ user, id, replyAt, getComments }) => {
+  const [loading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors } } = useForm({
+      mode: "onChange"
+    });
+
+  const onSubmit = async (data) => { };
+
+  return (
+    <form
+      className='w-full border-b border-[#66666645]'
+      onSubmit={handleSubmit(onSubmit)}>
+      <div className='w-full flex items-center gap-2 py-4'>
+        <img
+          src={user?.profileUrl ?? NoProfile}
+          alt="User Image"
+          className='w-10 h-10 rounded-full object-cover'
+        />
+        <TextInput
+          name="comment"
+          styles="w-full rounded-full py-3"
+          placeholder={replyAt ? `Reply @${replyAt}` : "Write a comment..."}
+          register={register("comment", {
+            required: "Comment can not be empty",
+          })}
+          error={errors.comment ? errors.comment.message : ""}
+        >
+        </TextInput>
+        <div className='flex items-center '>
+          {loading ? (
+            <Loading />
+          ) : (
+            <FaCircleArrowUp className='cursor-pointer' size={28} color="#0766FF" />
+          )}
+        </div>
+      </div>
+
+      {errMsg?.message && (
+        <span
+          role='alert'
+          className={`text-sm ${errMsg?.status === "failed"
+            ? "text-[#f64949fe]"
+            : "text-[#2ba150fe]"
+            } mt-0.5`}
+        >
+          {errMsg?.message}
+        </span>
+      )}
+    </form>
+  )
+};
 
 
+// ----------<  Post Container  >----------
 const PostCard = ({ post, user, deletePost, likePost }) => {
   const [showAll, setShowAll] = useState(0);
   const [showReply, setShowReply] = useState(0);
   const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [replyComments, setReplyComments] = useState(0);
   const [showComments, setShowComments] = useState(0);
 
   const getComments = async () => {
-
+    setReplyComments(0);
+    setComments(postComments);
+    setLoading(false);
   }
+
+  console.log(comments);
 
   return (
     <div className='mb-2 bg-primary p-4 shadown-sm border-b border-[#cccccc]'>
@@ -84,7 +152,8 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
           <p className='flex gap-1 items-center text-base cursor-pointer'>
             {post?.likes?.includes(user?._id) ? (
               <div className='flex items-center'>
-                <BiSolidLike size={20} color="#0766FF" className='mr-2' />
+                <BiSolidLike size={20} color="#0766FF" className='' />
+                <FcLike size={20} />
                 <span>You and</span>
               </div>
             ) : (
@@ -98,24 +167,25 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
           </p>
 
           <p className='flex gap-2 items-center text-base cursor-pointer'
-            onClick={() => {
-              setShowComments(showComments === post?._id ? null : post?._id);
-              getComments(post?._id);
-            }}
+
           ></p>
 
         </div>
 
-        {/* Reaction Action */}
+        {/* Action */}
         <div className='mt-4 flex justify-between items-center px-3 py-4 text-ascent-2 text-base border-t border-[#66666645]'>
           <div className='flex items-center cursor-pointer'>
             <BiSolidLike size={20} color="#0766FF" className='mr-2' />
             <span>Liked</span>
           </div>
 
-
-          <p className='flex gap-2 items-center text-base cursor-pointer'>
-            <GoCommentDiscussion />
+          <p
+            className='flex gap-2 items-center text-base cursor-pointer'
+            onClick={() => {
+              setShowComments(showComments === post._id ? null : post._id);
+              getComments(post?._id);
+            }}>
+            <GoCommentDiscussion size={20} />
             {post?.comments?.length} comments
           </p>
 
@@ -126,21 +196,68 @@ const PostCard = ({ post, user, deletePost, likePost }) => {
 
           {user?._id === post?.userId?._id && (
             <div
-              className='flex items-center'
+              className='flex items-center cursor-pointer'
               onClick={() => deletePost(post?._id)}
             >
               <MdOutlineDeleteOutline size={20} />
               <span>Delete</span>
             </div>
           )}
-
-
         </div>
-
-
-
       </div>
 
+      {/* Comment Section */}
+      {
+        showComments === post?._id && (
+          <div className='w-full mt-4 border-t border-[#66666645] pt-4'>
+            <CommentForm
+              user={user}
+              id={post?._id}
+              getComments={() => getComments(post?._id)}
+            />
+
+            {loading ?
+              (<Loading />) : comments?.length > 0 ? (
+                comments?.map((comment) => (
+                  //  Comment Container
+                  <div className='w-full py-2' key={comment?._id}>
+                    <div className='flex gap-3 items-center mb-1'>
+                      <Link to={"/profile/" + comment?.userId?._id}>
+                        <img
+                          src={comment?.userId?.profileUrl ?? NoProfile}
+                          alt={comment?.userId?.firstName}
+                          className="h-10 w-10 roundeful object-cover"
+                        />
+                      </Link>
+                      <div className=''>
+                        <Link to={"/profile/" + comment?.userId?._id}>
+                          <p className='font-medium text-base text-ascent'>
+                            {comment?.userId?.firstName} {comment?.userId?.lastName}
+                          </p>
+                        </Link>
+                        <span className='text-ascent-2 text-sm'>
+                          {moment(comment?.createdAt ?? "2024-9-01").fromNow()}
+                        </span>
+                      </div>
+                    </div>
+
+
+
+
+
+
+
+                  </div>
+                )
+                )
+              ) : (
+                <span className='flex text-sm py-4 text-ascent-2 text-center'>
+                  No comments yet. Be the first to comment.
+                </span>
+              )}
+          </div>
+
+        )}
 
 
 
