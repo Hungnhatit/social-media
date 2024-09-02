a25331f2e8ddeeeb6e0565914d727fbe79450cfd
Hungnhatit <nhatpro204@gmail.com>
2024-09-02 16:44:01 +0700

    Update edit profile page

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -39,7 +39,7 @@ const EditProfile = () => {
       <div className='fixed z-50 inset-0 overflow-y-auto'>
         <div className='flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
           <div className='fixed inset-0 transition-opacity'>
-            <div className='absolute inset-0 bg-[#000] opacity-70'>
+            <div className='absolute inset-0 bg-[#000] opacity-70' onClick={handleClose}>
             </div>
           </div>
           <span className='hidden sm:inline-block sm:align-middle sm:h-screen'></span>
