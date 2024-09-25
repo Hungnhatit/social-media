b4660efd5ddb16bbf4ffc675f561b7f0b2614e17
Hungnhatit <nhatpro204@gmail.com>
2024-09-25 10:41:42 +0700

    Update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -49,7 +49,7 @@ const Login = () => {
     <div className="bg-bgColor w-full h-[100vh] flex items-center justify-center p-6">
       <div className="w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl">
         {/* LEFT */}
-        <div className="w-1/2 lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
+        <div className="w-1/2 md:w-full sm:w-full xxs:w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center">
           <div className="w-full flex gap-2 items-center justify-center mb-6">
             <div className="p-2 bg-[#065ad8] rounded text-white">
               <TbSocial></TbSocial>
